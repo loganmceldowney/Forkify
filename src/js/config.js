d4f0df04e1a934adfe19a31a618f8653401c9082
loganmceldowney <daniel.mceldowney@ucdenver.edu>
2023-11-15 02:30:23 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6a319566-0b21-4886-ba63-1f56e6efe32e';
+export const MODAL_CLOSE_SEC = 2.5;
